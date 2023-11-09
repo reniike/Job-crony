51572747816ee44a500d3bb483705955dc92dd85
Babs-Olokode Aliyah <aliyaheniola91@gmail.com>
2023-11-09 01:07:49 +0100

    admin invitation test

--- src/test/java/com/example/jobcrony/services/adminService/AdminServiceImplTest.java
+++ src/test/java/com/example/jobcrony/services/adminService/AdminServiceImplTest.java
@@ -3,9 +3,11 @@ package com.example.jobcrony.services.adminService;
 import com.example.jobcrony.data.models.AdminInvitation;
 import com.example.jobcrony.data.repositories.AdminInvitationRepo;
 import com.example.jobcrony.data.repositories.AdminRepository;
+import com.example.jobcrony.dtos.requests.AdminInvitationRequest;
 import com.example.jobcrony.dtos.requests.AdminRegistrationRequest;
 import com.example.jobcrony.dtos.responses.GenericResponse;
 import com.example.jobcrony.exceptions.AdminExistException;
+import com.example.jobcrony.exceptions.SendMailException;
 import com.example.jobcrony.exceptions.UserNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +17,7 @@ import org.springframework.http.ResponseEntity;
 
 import java.util.Optional;
 
-import static com.example.jobcrony.utilities.AppUtils.ADMIN_REGISTERED_SUCCESSFULLY;
-import static com.example.jobcrony.utilities.AppUtils.HTTP_STATUS_OK;
+import static com.example.jobcrony.utilities.AppUtils.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -48,12 +49,18 @@ class AdminServiceImplTest {
                 );
 
         GenericResponse<String> invitation = adminService.registerAdmin(registrationRequest).getBody();
-        assert invitation != null;
+        assertNotNull(invitation);
         assertEquals(ADMIN_REGISTERED_SUCCESSFULLY, invitation.getMessage());
         assertEquals(HTTP_STATUS_OK, invitation.getStatus());
     }
 
     @Test
-    void sendInvitationLink() {
+    void sendInvitationLink() throws AdminExistException, SendMailException {
+        AdminInvitationRequest adminInvitationRequest = new AdminInvitationRequest();
+        adminInvitationRequest.setEmail("renikecodes@gmail.com");
+        GenericResponse<String> response = adminService.sendInvitationLink(adminInvitationRequest).getBody();
+        assertNotNull(response);
+        assertEquals(EMAIL_SENT_SUCCESSFULLY, response.getMessage());
+
     }
 }
